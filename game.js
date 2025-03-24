ecfb4924606de22eb440553f62a7701a7a90f0b3
Bryson Banks <brysonbanks04@gmail.com>
2025-03-23 20:35:51 -0500

    Game

--- game.js
+++ game.js
@@ -12,5 +12,47 @@ function initializeGame() {
 
 
 function playGame() {
-    document.getElementById("question").textContent = "Game started!";
+    if (questionCounter < questions.length) {
+        var currentQuestion = questions[questionCounter];
+
+    
+        document.getElementById("question").textContent = currentQuestion[0];
+
+    
+        var answersDiv = document.getElementById("answers");
+        answersDiv.innerHTML = "";
+
+        for (var i = 2; i < currentQuestion.length; i++) {
+            var answerButton = document.createElement("button");
+            answerButton.textContent = currentQuestion[i];
+            answerButton.onclick = (function(index) {
+                return function() {
+                    checkAnswer(index, currentQuestion[1]);
+                };
+            })(i - 2); 
+
+            
+            answersDiv.appendChild(answerButton);
+        }
+
+        
+        questionCounter++;
+    } else {
+
+        document.getElementById("prompt").innerHTML = "<p>Game Over!</p><button onclick='location.reload()'>Restart Game</button>";
+    }
 }
+
+
+function checkAnswer(selectedIndex, correctIndex) {
+    if (selectedIndex === correctIndex) {
+        alert("Correct!");
+    } else {
+        alert("Incorrect!");
+    }
+
+    
+    playGame();
+}
+
+window.onload = initializeGame;
