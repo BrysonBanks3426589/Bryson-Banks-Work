fb2bbe9c2d5a2e7dfc621808683b1dfe826d5c2d
Bryson Banks <brysonbanks04@gmail.com>
2025-03-31 18:42:28 -0500

    yes

--- jquery-intro.js
+++ jquery-intro.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
     // create a months array
-    var months = ['January', 'February','March','April','May','June','July','August','September','October','November', 'December'];
+    var months = 
+    ['January', 
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November', 
+    'December'];
     // create a tips array
     var tips=
     ["<p>Those of  us who live through our gardens know how bittersweet it can be to see the lush green growth of  summer disappear during the cold months of  winter. Hellebores are the perfect winter garden companion to plant with another winter blooming favorite, Camellias.</p><p>Visit us this season for the exciting selection of  both of  these cool season beauties and enjoy them in your garden for years to come.</p>",
