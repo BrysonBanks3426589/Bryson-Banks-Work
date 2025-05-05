859f62f676d84d86c6b012051e764d984d27be9d
Bryson Banks <brysonbanks04@gmail.com>
2025-05-04 23:23:49 -0500

    yes

--- orderform.js
+++ orderform.js
@@ -21,39 +21,36 @@ $(document).ready(function() {
    
     function calculateOrder() {
         let orderTotal = 0;
+        let hasItems = false;
         
         $('.qty').each(function() {
-            const index = $(this).attr('id').replace('qty', '');
+            const index = $(this).attr('id');
             let qty = parseInt($(this).val()) || 0;
-            $(this).val(qty); 
             
-            const price = parseFloat($('#price' + index).text().replace('$', ''));
+            if (qty > 0) hasItems = true;
+            
+            const price = parseFloat($('#price' + index).text().replace(/[^0-9.-]/g, ''));
             const total = price * qty;
             $('#total' + index).text('$' + total.toFixed(2));
-            
             orderTotal += total;
         });
         
         $('#subt').text('$' + orderTotal.toFixed(2));
         
         const state = $('#shipstate').val();
-        let tax = 0;
-        if (state === 'TX') {
-            tax = orderTotal * 0.0825;
-        }
+        let tax = state === 'TX' ? orderTotal * 0.0825 : 0;
         $('#tax').text('$' + tax.toFixed(2));
         
         let shipping = 10;
-        if (state === 'TX') {
-            shipping = 5;
-        } else if (state === 'CA' || state === 'NY') {
-            shipping = 20;
-        }
+        if (state === 'TX') shipping = 5;
+        else if (state === 'CA' || state === 'NY') shipping = 20;
         $('#ship').text('$' + shipping.toFixed(2));
         
-        $('#gTotal').text('$' + (orderTotal + tax + shipping).toFixed(2));
+        const grandTotal = orderTotal + tax + shipping;
+        $('#gTotal').text('$' + grandTotal.toFixed(2));
+        
+        return hasItems; 
     }
-
     
     $('.qty').on('change keyup blur', calculateOrder);
     $('#state, #shipstate').change(calculateOrder);
